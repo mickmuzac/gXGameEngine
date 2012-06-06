e7a0e7fde904030a282a2c9b4e133738d253aec6
unknown <Mick@Mick-PC.(none)>
2012-06-06 16:18:54 -0400

    Added image directory, bug fixes.

--- scripts/spriteMap.js
+++ scripts/spriteMap.js
@@ -3,6 +3,6 @@
 	they must have a global scope.
 */
 
-var sMap = new SpriteMap("moresprites.PNG",900, 50, 9, 14*4, 3, 4);
-var sMap2 = new SpriteMap("a.gif", 0, 50, 4, 1, 4, 1);
-var sMap3 = new SpriteMap("tree.png", 500, 180, 1, 1, 1, 1);
+var sMap = new SpriteMap("images/moresprites.PNG",900, 50, 9, 14*4, 3, 4);
+var sMap2 = new SpriteMap("images/a.gif", 0, 50, 4, 1, 4, 1);
+var sMap3 = new SpriteMap("images/tree.png", 500, 180, 1, 1, 1, 1);
